48be511b5cc92d6d63a8d9c65554cfa946fe0c8c
dummassdenzel <denzelmanzperez1@gmail.com>
2023-08-26 21:11:27 +0800

    Co-authored-by: Estrella <Amanogawaa@users.noreply.github.com>

---
+++ Game/Misc.cs
@@ -0,0 +1,7 @@
+namespace PokemonGame{
+    public class Misc{
+        public static void Line(){
+            Console.WriteLine("---------------------------------------------------------------------------");
+        }
+    }
+}
